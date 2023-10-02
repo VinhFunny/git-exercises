e10ef7f20cc54c45a6c15418fbb6e9f2c75df528
Hien Vinh <huahienvinh2306@icloud.com>
2023-10-02 17:35:38 +0700

    Modify helloworld.py.file

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,5 @@
 #! /usr/bin/python3
 print("Hello world")
-<<<<<<< HEAD
-print("Hello Git")
+print("Hello OSS Develoment")
 print("Hello CTU")
-=======
-print("Hello OSS Development")
-print("Hello CTU - CICT")
->>>>>>> test_functions
+
